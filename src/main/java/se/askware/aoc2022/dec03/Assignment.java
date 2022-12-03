a06d70de588278b50011c2b85cc16f1e06fc28e4
Andreas Skoog <andreas.skoog@avanza.se>
2022-12-03 08:56:36 +0100

    day 3

--- src/main/java/se/askware/aoc2022/dec03/Assignment.java
+++ src/main/java/se/askware/aoc2022/dec03/Assignment.java
@@ -13,10 +13,44 @@ public class Assignment extends AocBase {
 
 	@Override
 	public void solvePartOne(List<String> input) {
+		int sum = 0;
+		for (String s : input) {
+			String first = s.substring(0, s.length() / 2);
+			String second = s.substring(s.length() / 2);
+			for (char c : first.toCharArray()){
+				if (second.indexOf(c) >= 0){
+					if (Character.isLowerCase(c)) {
+						sum += c - 'a' + 1;
+					}else{
+						sum += c - 'A' + 27;
+					}
+					break;
+				}
+			}
+		}
+		System.out.println(sum);
 	}
 
 	@Override
 	public void solvePartTwo(List<String> input) {
+		int sum = 0;
+		for (int i = 0; i < input.size(); i+=3) {
+			String s1 = input.get(i);
+			String s2 = input.get(i+1);
+			String s3 = input.get(i+2);
+
+			for (char c : s1.toCharArray()){
+				if (s2.indexOf(c) >= 0 && s3.indexOf(c) >= 0){
+					if (Character.isLowerCase(c)) {
+						sum += c - 'a' + 1;
+					}else{
+						sum += c - 'A' + 27;
+					}
+					break;
+				}
+			}
+		}
+		System.out.println(sum);
 	}
 
 }
