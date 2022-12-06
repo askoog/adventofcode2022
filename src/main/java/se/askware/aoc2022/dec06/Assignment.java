efcf30dd2bf367e099d7c8636bbfb22b283c2a04
Andreas Skoog <andreas.skoog@avanza.se>
2022-12-06 07:31:52 +0100

    day 6

--- src/main/java/se/askware/aoc2022/dec06/Assignment.java
+++ src/main/java/se/askware/aoc2022/dec06/Assignment.java
@@ -13,10 +13,40 @@ public class Assignment extends AocBase {
 
 	@Override
 	public void solvePartOne(List<String> input) {
+		for (String s : input) {
+			int len = 4;
+			findFirstUniqueString(s, len);
+		}
+	}
+
+	private static void findFirstUniqueString(String s, int len) {
+		for (int i = 0; i < s.length() - 4; i++) {
+			String sub = s.substring(i, i + len);
+			if (!containsDuplicateCharacters(sub)) {
+				System.out.println(i + len);
+				return;
+			}
+		}
+	}
+
+	private static boolean containsDuplicateCharacters(String sub) {
+		for (int j = 0; j < sub.length(); j++) {
+			char c = sub.charAt(j);
+			for (int k = j + 1; k < sub.length(); k++) {
+				if (sub.charAt(k) == c) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	@Override
 	public void solvePartTwo(List<String> input) {
+		for (String s : input) {
+			int len = 14;
+			findFirstUniqueString(s, len);
+		}
 	}
 
 }
